6333b6ca7a8a827408df3a66cb211bfd1edef35b
tae yoon <taenaive@gmail.com>
2014-07-01 01:35:17 -0400

    write test failed

--- test/soapTest.js
+++ test/soapTest.js
@@ -2,7 +2,12 @@
   var soap = require('soap');
   var url = 'http://198.135.15.18:8001/soa-infra/services/default/DocumentUploadDetailsService/DocumentUploadDetailsService_ep?WSDL';
   var ep_uri ='http://198.135.15.18:8001/soa-infra/services/default/DocumentUploadDetailsService/DocumentUploadDetailsService_ep'
-  var args = {retrieveDocumentRequest: {requestHeader:{ loginUserName: 'null', requestId:'1'}}, applicantId:'1111'};
+  var retrieveArgs = {requestHeader:{ loginUserName: 'null', requestId:'1'}, applicantId:'1111'};
+  var saveArgs = {requestHeader:{ loginUserName: 'null', requestId:'1'}, 
+                                        applicantId: '1111',
+                                        answerId: '41c',
+                                        dDocName: 'dID=1858;dDocName=MPSTD001858',
+                                        createdBy: 'nodejs'};
   console.log("trying to connect...");
   soap.createClient(url, function(err, client) {
   	  if(err){
@@ -10,9 +15,17 @@
   	  }
   	  else{
 	  	  console.log("       ======= Client  Describe output: ======");
-	  	  console.log(client.describe());
-	      client.retrieveDocument(args, function(err, result) {
-	          console.log(result.DocumentCollection.Document);
-      });
+	  	  console.log(client.describe().DocumentUploadDetailsService_ep.DocumentUploadDetailsServicePort_pt.saveDocument);
+        //saveDocument , retrieveDocument
+        //console.log( JSON.stringify(client) );
+        client.setSecurity(new soap.BasicAuthSecurity('tae', 'Welcome1')); 
+	       client.retrieveDocument(retrieveArgs, function(err, result) {
+	            console.log(result.DocumentCollection.Document);
+          });
+
+        client.saveDocument(saveArgs, function(err, result) {
+           console.log(result.body);
+        });
+
       }
   });
